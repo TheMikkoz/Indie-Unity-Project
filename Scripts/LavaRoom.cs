4dacc534258849e4ab6a40919f53fe44907bf69a
themikkoz <mikko.jyrkas@hotmail.com>
2020-10-24 15:45:53 +0300

    !Upload

---
+++ Scripts/LavaRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaRoom : MonoBehaviour
+{
+    public Handler Hndlr;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            Hndlr.Respawn();
+        }
+    }
+}
